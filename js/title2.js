3966d3b686c33ac40dbc74d81854f25a1b9ce409
mr-flintstone <39106277+mr-flintstone@users.noreply.github.com>
2019-02-06 19:31:07 -0800

    Add files via upload
    fix opening channels attempt 2

---
+++ js/title2.js
@@ -0,0 +1,16 @@
+title_maker();
+function title_maker() {
+
+  var github = document.createElement("p");
+
+  github.innerHTML = "<a href=\"https://github.com/mr-flintstone/light-node-amoveo/blob/master/README.md\" target='blank' >Instructions</a>";
+  document.body.appendChild(github);
+
+
+    var github2 = document.createElement("p");
+    github2.innerHTML = "<a href=\"https://github.com/zack-bitcoin/amoveo/blob/master/README.md\" target='blank' >Learn more about Amoveo</a>";
+    document.body.appendChild(github2);
+
+
+
+};
